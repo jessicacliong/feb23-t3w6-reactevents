fed4ce68bbf254ca233260f38554f661bac4f3b2
jessicacliong <14395@coderacademy.edu.au>
2023-09-19 19:50:35 +1000

    feat: app turned into a class

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import React from 'react'
+import './App.css';
+
+// Page that lets us increase or decrease
+// how many Pokemon are on screen 
+// App component needs to:
+// - keep track of how many Pokemon to show 
+// - iterate over a single, D.R.Y modular Pokemon component 
+// - button to increase Pokemon count
+// - button to decrease Pokemon count 
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    // start with at least ONE pokemon on screen 
+    this.state = {
+      pokemonCount: 1
+
+    }
+  }
+  render(){
+    return (
+      <div>
+        <h1> Pokemon Page </h1>
+      </div>
+    )
+  }
+
+}
+
+export default App;
